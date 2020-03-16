df61ed94d6c494ab2954e83c8a7684ed6a616bba
achugh04 <chughabhiishek@gmail.com>
2020-03-16 16:22:01 +0530

    modified:   functionheader.h 	modified:   functions.c 	modified:   main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ dyna_var *head=NULL;
 void main()
 {	
 	FILE *fp;
-	char *input_string,**w,buf[1024],**t,*cwd;
+	char *input_string,**w,buf[1024],**t=NULL,*cwd;
 	int j,status;
 
 
@@ -16,13 +16,10 @@ void main()
 
 	while(1)
 	{	
-		//for(int i=0;i<10;i++)
-		//	t[i]=NULL;
 		if (wait(&status))
 		{
 			cwd=getcwd(buf,sizeof(buf));
 			input_string=readline("~/cherry_shell/:)/>>> ");
-			//input_string="9+9+9";
 			add_history(input_string);	
 			char *pipe_position=check_for_pipes(&input_string);
 			/*if (pipe_position!=NULL)
@@ -30,32 +27,20 @@ void main()
 				t=tokenizer_for_pipes(s);
 				goto here;
 			}*/
-
-			/*int command_size=sizeof(s);
-			int command_fp_position=ftell(fp);
-			command_history_head=insert(command_history_head,command_size,command_fp_position);
-			fwrite(s,command_size,1,fp);
-			char *history_command=history_call(command_history_head,&fp);
-			printf("%s\n\n\n\n\n\n",history_command);
-*/
-			int i=0;
 		
 			t=tokenizer_for_all(input_string);
 
-here:
+			int i=0;
 			i=check_function_call(t);
-/*	char *x[]={"9","+","6","+","6","#"};
-		w=Infix_to_Suffix(x);
-		printf("%c\n",w[0][0]);
-*/
+			
 			switch(i)
 			{
 				case 1:
 					j=fork();
 					if (j==0)
 					{
-						execlp(t[0],t[0],t[1],t[2],t[3],t[4],t[5],t[6],t[7],t[8],t[9],t[10],NULL);
-						//perror("execlp");
+						execlp(t[0],t[0],t[1],t[2],t[3],t[4],t[5],NULL);
+						perror("execlp");
 					}
 					break;
 				case 2:
@@ -83,7 +68,7 @@ here:
 					{
 						w=Infix_to_Suffix(t);
 						double exp=Evaluate_Suffix_Expression(w);
-						printf("~/cherry_shell/:)/>>> %.4lf\n",exp);
+						printf("~/cherry_shell/:)/>>> %.3lf\n",exp);
 						perror("Suffix Evaluation");
 						exit(1);
 					}
@@ -96,7 +81,7 @@ here:
 					{
 						w=Infix_to_Suffix(t);
 						double result=operations(head,w);
-						printf("~/cherry_shell/:)/>>> %.4lf\n",result);
+						printf("~/cherry_shell/:)/>>> %.3lf\n",result);
 						perror("Variable Suffix Evaluation");
 						exit(1);
 					}
